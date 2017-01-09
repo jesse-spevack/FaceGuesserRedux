59537924ce18c149d0b5432359b09ab20427d10c
PlanetEfficacy <jspevack@gmail.com>
2017-01-08 17:05:33 -0700

    Add score to game

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import CohortSelect from './CohortSelect'
-import Game from './Game'
-import $ from 'jquery'
+import React          from 'react';
+import CohortSelect   from './CohortSelect'
+import Game           from './Game'
+import axios          from 'axios'
+import _              from 'lodash'
 
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super()
-    this.state = { cohort: '' }
+    this.state = { cohort: '', correct: 0, incorrect: 0 }
   }
 
   startGame(cohort) {
-    $.getJSON('http://localhost:3001/api/v1/users', (people) => {
-      this.setState( { people: people } )
-    }).then(() => {
-      this.setState( { cohort: cohort } )
+    axios.get('http://localhost:3001/api/v1/users')
+    .then((response) => {
+      return _.filter(response.data, (person) => {
+        return person.cohort === cohort;
+      })
     })
+    .then((peopleFromCohort) => {
+      this.setState( { people: peopleFromCohort, cohort: cohort })
+    });
   }
+
+  incrementCorrect() {
+    this.setState({ correct: this.state.correct += 1 })
+  }
+
+  incrementIncorrect() {
+    let incorrect = this.state.incorrect += 1
+    this.setState({ incorrect: incorrect })
+  }
+
 
   render() {
     if (this.state.cohort === '') {
@@ -26,7 +41,12 @@ class App extends Component {
       )
     } else {
       return (
-        <Game people={ this.state.people }/>
+        <div>
+          <h4>Correct: {this.state.correct} Incorrect: {this.state.incorrect}</h4>
+          <Game people={this.state.people}
+                correct={() => this.incrementCorrect()}
+                incorrect={() => this.incrementIncorrect()}/>
+        </div>
       )
     }
   }
--- src/Game.js
+++ src/Game.js
@@ -1,20 +1,72 @@
-import React from 'react';
-var _ = require('lodash');
+import                React from 'react';
+import _              from 'lodash'
 
 class Game extends React.Component {
-  getRandomPerson() {
-    // console.log(this.props.people)
-    return _.sample(this.props.people)
+  constructor(props) {
+    super(props)
+    this.state = { person: this.getRandomPerson() };
   }
 
+  componentWillMount() {
+    let randomFour = this.getRandomFour(this.state.person);
+    this.setState( { choices: randomFour })
+  }
+
+  getRandomPerson() { return _.sample(this.props.people) }
+
+  getRandomFour(person) {
+    console.log("Person = " + this.state.person.first_name)
+    let randomFour = [person];
+    while (randomFour.length < 4 ) {
+      let person = _.sample(this.props.people)
+      if (randomFour.indexOf(person) === -1) {
+        randomFour.push(person)
+      }
+    }
+    console.log("Random 4 = " + randomFour[0].first_name + " " + randomFour[1].first_name + " " + randomFour[2].first_name + " " + randomFour[3].first_name)
+    return randomFour;
+  }
+
+  makeGuess(event) {
+    let guess = event.target.text
+    if ( guess === this.state.person.first_name ) {
+      this.props.correct()
+    } else {
+      this.props.incorrect()
+    }
+    this.nextTurn()
+  }
+
+  nextTurn() {
+    let person = this.getRandomPerson();
+    let randomFour = this.getRandomFour(person);
+    this.setState({ person: person, choices: randomFour })
+  }
+
+
   render() {
+    let choices = _.map(this.state.choices, (choice) => {
+      return (
+        <div key={choice.id} className="row">
+          <a href="#"
+             className="btn btn-info btn-lg col-md-6"
+             onClick={ (event) => this.makeGuess(event) } >
+              {choice.first_name}
+             </a>
+        </div>
+      )
+    })
     return (
       <div>
-        <img src={this.getRandomPerson().image_url} />
-        <a href="#" className="btn btn-default">{this.getRandomPerson().first_name}</a>
-        <a href="#" className="btn btn-default">{this.getRandomPerson().first_name}</a>
-        <a href="#" className="btn btn-default">{this.getRandomPerson().first_name}</a>
-        <a href="#" className="btn btn-default">{this.getRandomPerson().first_name}</a>
+        <div className="container">
+          <div className="col-md-6">
+            <img src={this.state.person.image_url} />
+            <p>{this.state.person.first_name}</p>
+          </div>
+          <div className="col-md-6">
+            {choices}
+          </div>
+        </div>
       </div>
     )
   }
